5da99461495a0a7fb2c02c368968836d381b2c93
Jackson Henry <jackson.henry13@yahoo.com>
2019-07-22 21:39:43 -0500

    Moved all files to TravelToChina folder.

---
+++ src/title/Title.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+let Title = () => {
+  return (
+    <div className="container">
+      <div className="jumbotron">
+        <h1>Travel To China</h1>
+        <p> ----------------- </p>
+      </div>
+      {}
+    </div>
+  );
+};
+
+export default Title;
