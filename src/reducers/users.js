8dbca87b483c2c24c7ba0156e7b6eeb045d5e0d7
Jackson Henry <jackson.henry13@yahoo.com>
2019-07-27 12:38:35 -0500

    Working on getting this project to work with redux. That is the purpose of the users and store js files.

---
+++ src/reducers/users.js
@@ -0,0 +1,18 @@
+import {createSlice} from "redux-starter-kit";
+
+export const users = createSlice ({
+    slice: "users", 
+    initialState: {
+        name: "Unknown"
+    }, 
+    reducers: {
+        "login": (state, action) => {
+            state.name = action.payload.name;
+        },
+        "logout": (state, action) => {
+            state.name = "Unknown";
+        }
+    }
+});
+
+export default users.reducer;
